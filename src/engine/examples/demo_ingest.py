b34e814187ca844fb04306d3c5ae36bd6017575e
dj953590 <69596727+dj953590@users.noreply.github.com>
2025-06-25 18:15:54 -0400

    #9 separate the ingest with query

---
+++ src/engine/examples/demo_ingest.py
@@ -0,0 +1,49 @@
+import os
+import time
+
+from src.engine.graph import GraphEngine, QueryParam
+from src.llm.oai import gpt_4o_mini_complete, llama_3_3_70b_turbo, deepseek_distill_llama
+from src.llm.oai import llama_3_3_70b_versatile
+from dynaconf import settings
+from src.docs.parser.pdf_processor import PDFProcessor
+from pathlib2 import Path
+
+from src.utils.utils import compute_mdhash_id
+relationship = "gxo"
+WORKING_DIR = "./" + relationship
+doc_name = "citibank-" + relationship
+
+if not os.path.exists(WORKING_DIR):
+    os.mkdir(WORKING_DIR)
+pdf_path = (
+        Path(__file__).parent / relationship / (doc_name + ".pdf")
+)  # Replace with your PDF file path
+
+output_path = Path(__file__).parent / relationship / (doc_name + ".json")  # Output JSON file
+output_text_path = Path(__file__).parent / relationship / (doc_name + ".txt")  # Output JSON file
+
+doc_id = compute_mdhash_id(doc_name, "DOC-")
+
+
+# print the start time
+start_time = time.time()
+print(f"Processing document: {doc_name} with ID: {doc_id} at {pdf_path}")
+
+os.environ["OPENAI_API_KEY"] = settings.get("OPENAI_API_KEY")
+engine = GraphEngine(
+    working_dir=WORKING_DIR,
+    llm_model_func=gpt_4o_mini_complete,
+    doc_id=doc_id,
+    doc_name=doc_name,
+)
+
+extractor = PDFProcessor(file_path=pdf_path, output_path=output_path, output_text_path=output_text_path)
+structured_data = extractor.markdown()
+engine.insert(structured_data)
+
+# Record the end time
+end_time = time.time()
+
+# Calculate and print the total processing time
+total_time = end_time - start_time
+print(f"Total processing time: {total_time:.2f} seconds")
